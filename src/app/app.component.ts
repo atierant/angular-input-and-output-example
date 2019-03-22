e4d2eaf1711936cceb044b5dc19258f96bde863a
atierant <arnaud@tierant.fr>
2019-03-22 17:38:33 +0100

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
+export class AppComponent {
+
   name = 'Angular 6';
+
   dataFromChild;
+
+  /**
+   * J'écoute les événements de l'event emitter de mon enfant
+   */
   eventFromChild(data) {
     this.dataFromChild = data;
   }
--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,18 +1,30 @@
-import { Component, Input, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, Input, EventEmitter, Output, OnChanges, SimpleChanges, SimpleChange } from '@angular/core';
 
 @Component({
   selector: 'child-component',
   templateUrl: './child.component.html',
 })
-export class ChildComponent implements OnInit {
+export class ChildComponent implements OnChanges {
+
+  // Input car donnée provenant de l'extérieur
   @Input() dataFromParent: String;
+
+  // Output car sera écouté de l'extérieur
   @Output() sendDataToParent = new EventEmitter<string>();
 
-  ngOnInit() {
-    console.log('This is data from parent', this.dataFromParent);
+  _sendDataToParent(data: string) {
+    this.sendDataToParent.emit(data);
   }
 
-  _sendDataToParent(data:string) {
-    this.sendDataToParent.emit(data);
+  ngOnChanges(changes: SimpleChanges) {
+    // J'écoute les changements de ma propriété dataFromParent
+    // bindée à dataFromChild dans le template de mon parent
+    const dataFromParent: SimpleChange = changes.dataFromParent;
+    if(dataFromParent.currentValue) {
+      console.log('previous dataFromParent value: ', dataFromParent.previousValue);
+      console.log('current dataFromParent value : ', dataFromParent.currentValue);
+      console.log('---------------');
+      this.dataFromParent = dataFromParent.currentValue;;
+    }
   }
 }
